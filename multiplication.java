9d7682fa40d4339a7aafe91647642a67fca5adf4
Ajay Kumar Behera <ajaykumarbehera808@gmail.com>
2025-07-09 15:04:52 +0530

    multiplication

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+public class multiplication {
+     {
+    public static void main(String[] args) {
+        int num1 = 5; // You can change this value
+        int num2 = 3; // You can change this value
+
+        int product = num1 * num2;
+        System.out.println("Multiplication: " + product);
+    }
+}
+    
+}
