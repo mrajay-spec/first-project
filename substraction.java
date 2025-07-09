78d2bff7c928dd1c801e596a96d9181f06cd98a7
Ajay Kumar Behera <ajaykumarbehera808@gmail.com>
2025-07-09 14:42:37 +0530

    substractionupdate

---
+++ substraction.java
@@ -0,0 +1,13 @@
+public class substraction {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+
+        System.out.println("Before subtraction: a = " + a + ", b = " + b);
+
+        // Subtracting b from a
+        int result = a - b;
+
+        System.out.println("After subtraction: result = " + result);
+    }
+}
